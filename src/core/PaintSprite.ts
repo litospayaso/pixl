6c5a4da112b0a7f9efe64429e4cf38b44be2a9e0
Hita <697729@cognizant.com>
2018-10-24 10:08:50 +0200

    creating global paint sprite

--- src/core/PaintSprite.ts
+++ src/core/PaintSprite.ts
@@ -1,37 +1,45 @@
-let sheet;
-let newTexture;
-let context;
+import {LevelProperties} from './LevelProperties';
 
-export const initPaintSprite = (props: Phaser.Scene) => {
-    sheet = props.textures.get('player').getSourceImage();
-    newTexture = props.textures.createCanvas('playerUpdate', sheet.width, sheet.height);
-    context = newTexture['getContext']('2d');
-    context.drawImage(sheet, 0, 0);
-};
+let isCreated = false;
 
-export const paintSprite = (color: string) => {
-    const imageData = context.getImageData(0, 0, sheet.width, sheet.height);
-    const pixelArray = imageData.data;
+export const paintSprite = {
+    sheet: undefined,
+    newTexture: undefined,
+    context: undefined,
+    init(props: LevelProperties) {
+        if (!isCreated) {
+            this.sheet = props.scene.textures.get('player').getSourceImage();
+            this.newTexture = props.scene.textures.createCanvas('playerUpdate', this.sheet.width, this.sheet.height);
+            this.context = this.newTexture['getContext']('2d');
+            this.context.drawImage(this.sheet, 0, 0);
+            isCreated = true;
+        }
+    },
+    paint(color: string) {
+        const imageData = this.context.getImageData(0, 0, this.sheet.width, this.sheet.height);
+        const pixelArray = imageData.data;
 
-    const newPixelArray = [];
-    for (let i = 0; i < pixelArray.length; i = i + 4) {
-        const r = pixelArray[i];
-        const g = pixelArray[i + 1];
-        const b = pixelArray[i + 2];
-        const alpha = pixelArray[i + 3];
+        const newPixelArray = [];
+        for (let i = 0; i < pixelArray.length; i = i + 4) {
+            const r = pixelArray[i];
+            const g = pixelArray[i + 1];
+            const b = pixelArray[i + 2];
+            const alpha = pixelArray[i + 3];
+            // tslint:disable-next-line:max-line-length
+            newPixelArray.push(`${r.toString(16).length === 1 ? '0'.concat(r.toString(16)) : r.toString(16)}${g.toString(16).length === 1 ? '0'.concat(g.toString(16)) : g.toString(16)}${b.toString(16).length === 1 ? '0'.concat(b.toString(16)) : b.toString(16)}${alpha.toString(16).length === 1 ? '0'.concat(alpha.toString(16)) : alpha.toString(16)}`);
+        }
         // tslint:disable-next-line:max-line-length
-        newPixelArray.push(`${r.toString(16).length === 1 ? '0'.concat(r.toString(16)) : r.toString(16)}${g.toString(16).length === 1 ? '0'.concat(g.toString(16)) : g.toString(16)}${b.toString(16).length === 1 ? '0'.concat(b.toString(16)) : b.toString(16)}${alpha.toString(16).length === 1 ? '0'.concat(alpha.toString(16)) : alpha.toString(16)}`);
-    }
-    newPixelArray.forEach((e, i) => newPixelArray[i] = e === '000000ff' ? color : e);
+        const backgroundColor = newPixelArray.slice(0).sort((a, b) => newPixelArray.slice(0).filter((v) => v === a).length - newPixelArray.slice(0).filter((v) => v === b).length).pop();
+        newPixelArray.forEach((e, i) => newPixelArray[i] = e === backgroundColor ? color : e);
 
-    for (let i = 0; i < pixelArray.length; i = i + 4) {
-        pixelArray[i] = parseInt(newPixelArray[i / 4].slice(0, 2), 16);
-        pixelArray[i + 1] = parseInt(newPixelArray[i / 4].slice(2, 4), 16);
-        pixelArray[i + 2] = parseInt(newPixelArray[i / 4].slice(4, 6), 16);
-        pixelArray[i + 3] = parseInt(newPixelArray[i / 4].slice(6, 8), 16);
-    }
-
-    context.putImageData(imageData, 0, 0);
-
-    newTexture.refresh();
+        for (let i = 0; i < pixelArray.length; i = i + 4) {
+            pixelArray[i] = parseInt(newPixelArray[i / 4].slice(0, 2), 16);
+            pixelArray[i + 1] = parseInt(newPixelArray[i / 4].slice(2, 4), 16);
+            pixelArray[i + 2] = parseInt(newPixelArray[i / 4].slice(4, 6), 16);
+            pixelArray[i + 3] = parseInt(newPixelArray[i / 4].slice(6, 8), 16);
+        }
+        this.context.putImageData(imageData, 0, 0);
+        this.newTexture.refresh();
+    },
+    refresh: () => this.newTexture.refresh(),
 };
--- src/scenes/level1/configPlayer.ts
+++ src/scenes/level1/configPlayer.ts
@@ -1,55 +1,15 @@
 import { LevelProperties } from '../../core/LevelProperties';
+import { paintSprite } from '../../core/PaintSprite';
 
 export const ConfigPlayer = function(props: LevelProperties) {
-    // console.log(props.scene);
-    // props.player = new PlayerObject(props.scene, 100, 1100, 'dude');
-    // props.player.visible = false;
-    // props.player = this.physics.add.sprite(100, 1100, 'dude');
-    // props.player.playerHitted = false;
-    // props.player.blockPlayer = false;
 
-    const sheet = props.scene.textures.get('player').getSourceImage();
-    const newTexture = this.textures.createCanvas('playerUpdate', sheet.width, sheet.height);
-
-    const context = newTexture['getContext']('2d');
-    context.drawImage(sheet, 0, 0);
+    paintSprite.init(props);
 
     props.player = this.physics.add.sprite(100, 1100, 'playerUpdate');
     props.player.playerHitted = false;
     props.player.blockPlayer = false;
 
-    // const canvasTexture = props.scene.game.textures.createCanvas('player-update', sheet.width, sheet.height);
-    // const canvas = canvasTexture.getSourceImage();
+    paintSprite.paint('ff0000ff');
+    paintSprite.paint('00ff00ff');
 
-    const imageData = context.getImageData(0, 0, sheet.width, sheet.height);
-    const pixelArray = imageData.data;
-
-    const newPixelArray = [];
-    for (let i = 0; i < pixelArray.length; i = i + 4) {
-        const r = pixelArray[i];
-        const g = pixelArray[i + 1];
-        const b = pixelArray[i + 2];
-        const alpha = pixelArray[i + 3];
-        // tslint:disable-next-line:max-line-length
-        newPixelArray.push(`${r.toString(16).length === 1 ? '0'.concat(r.toString(16)) : r.toString(16)}${g.toString(16).length === 1 ? '0'.concat(g.toString(16)) : g.toString(16)}${b.toString(16).length === 1 ? '0'.concat(b.toString(16)) : b.toString(16)}${alpha.toString(16).length === 1 ? '0'.concat(alpha.toString(16)) : alpha.toString(16)}`);
-    }
-    // tslint:disable-next-line:max-line-length
-    const backgroundColor = newPixelArray.slice(0).sort((a, b) => newPixelArray.slice(0).filter((v) => v === a).length - newPixelArray.slice(0).filter((v) => v === b).length).pop();
-    newPixelArray.forEach((e, i) => newPixelArray[i] = e === backgroundColor ? 'ff0000ff' : e);
-
-    for (let i = 0; i < pixelArray.length; i = i + 4) {
-        pixelArray[i] = parseInt(newPixelArray[i / 4].slice(0, 2), 16);
-        pixelArray[i + 1] = parseInt(newPixelArray[i / 4].slice(2, 4), 16);
-        pixelArray[i + 2] = parseInt(newPixelArray[i / 4].slice(4, 6), 16);
-        pixelArray[i + 3] = parseInt(newPixelArray[i / 4].slice(6, 8), 16);
-    }
-
-    context.putImageData(imageData, 0, 0);
-
-    newTexture.refresh();
-
-    // sprite.update();
-    // sprite.refreshBody();
-    // sprite.setTintFill(0xFF0000);
-    // sprite.setMask(mask);
 };
