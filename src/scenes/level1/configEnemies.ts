a8968daf74083d9d9b0cd79c321d5e0b726ddf4f
Hita <697729@cognizant.com>
2019-02-01 15:30:04 +0100

    updating spikes

--- src/scenes/level1/configEnemies.ts
+++ src/scenes/level1/configEnemies.ts
@@ -5,10 +5,10 @@ export const ConfigEnemies = function(props: LevelProperties) {
     props.enemies.create(520, 970, 'enemiiixls');
 
     props.spikes.create(900, 1164, 'spikes');
-    props.spikes.create(930, 1164, 'spikes');
-    props.spikes.create(960, 1164, 'spikes');
-    props.spikes.create(990, 1164, 'spikes');
-    props.spikes.create(1020, 1164, 'spikes');
+    props.spikes.create(934, 1164, 'spikes');
+    props.spikes.create(968, 1164, 'spikes');
+    props.spikes.create(1002, 1164, 'spikes');
+    props.spikes.create(1036, 1164, 'spikes');
 
     props.enemies.children.iterate((enemy: Phaser.Physics.Arcade.Sprite) => {
         enemy.body.velocity.x = -100;
