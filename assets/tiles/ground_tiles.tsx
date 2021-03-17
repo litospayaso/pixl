d52964486f0b0ae0b501678960fb0800cecf9e70
Angel Hita <a.hita@talmundo.com>
2021-03-17 19:10:47 +0100

    Updates

--- assets/tiles/ground_tiles.tsx
+++ assets/tiles/ground_tiles.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="ground_tiles" tilewidth="32" tileheight="32" tilecount="50" columns="10">
- <image source="new-tiles.png" width="320" height="160"/>
+<tileset version="1.4" tiledversion="1.4.3" name="ground_tiles" tilewidth="48" tileheight="48" tilecount="162" columns="18">
+ <image source="ground-pixl.png" width="864" height="432"/>
 </tileset>
